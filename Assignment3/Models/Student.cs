a5f38f841996f343e6c2ea78b6c8bbf235d97cbe
Johan <Johan@DESKTOP-G5FFIEQ>
2020-11-16 14:32:43 +0100

    Student added in models

---
+++ Assignment3/Models/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment3.Models
+{
+
+   // <summary>description
+   //Grade for the student
+   //Added all possible content, maybe need to clean it up later
+   //</summary>
+    public class Student
+    {
+        public int CourseNumber { get; set; }
+        public string CoureModule { get; set; }
+        public int Id {get; set;}
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string GradeCanvas { get; set; }
+        public string ExamNumber { get; set; }
+        public string Infomartion { get; set; }
+        public string examDate { get; set; }
+        public string GradeLadok { get; set; }
+        public string Status { get; set; }
+
+
+    }
+}
