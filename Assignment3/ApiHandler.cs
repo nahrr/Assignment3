817096ab58d7df7499c46b079bd83daf39c32a67
Johan <Johan@DESKTOP-G5FFIEQ>
2020-11-17 16:42:22 +0100

    Nya klasser

---
+++ Assignment3/ApiHandler.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Net;
+using System.Net.Http;
+
+namespace Assignment3
+{
+    public class ApiHandler
+    {
+        private readonly string _courseCode;
+        private readonly string _courseModule;
+        
+ 
+
+
+
+        public ApiHandler(string courseModule, string courseCode)
+        {
+            _courseModule = courseModule;
+            _courseCode = courseCode;
+
+        }
+
+        /// <summary>
+        /// Tries to register result
+        /// </summary>
+        /// <returns>Result of registration</returns>
+        private string ViewResult()
+        {
+            var result = ConnectToAPI("http://localhost:51526/api/GetCourse", $"?courseModule={_courseModule}&courseCode={_courseCode}")
+                .Replace("\"", string.Empty); // Tar bort "" från svaret
+
+            return result;
+
+        }
+
+        /// <summary>
+        /// Conencts to an API and returns result
+        /// </summary>
+        /// <param name="url">URL of API</param>
+        /// <param name="urlParameters">Parameters</param>
+        /// <returns>Result or "invalid" if there's no response</returns>
+        private string ConnectToAPI(string url, string urlParameters)
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri(url);
+
+            var response = client.GetAsync(urlParameters).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                var result = response.Content.ReadAsStringAsync().Result;
+                return result; // Ska inte returnera, ska bara skriva in värde i _registrationCode
+
+            }
+            else
+            {
+                return "invalid";
+            }
+        }
+
+    }
+}
---
+++ Assignment3/Models/Course.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment3.Models
+{
+    public class Course
+    {
+        public string CourseCode { get; set; }
+        public string CourseName { get; set; }
+        public string CourseModule { get; set; }
+
+        public Course(string coursecode, string coursename, string coursemodule)
+        {
+            CourseCode = coursecode;
+            CourseName = coursename;
+            CourseModule = coursemodule;
+        }
+
+        public static List<Course> MadeUpCourses =>
+            new List<Course>
+            {
+                new Course ("1", "Economics", "Assignments"),
+                new Course ("2", "History", "Assignments"),
+                new Course ("3", "Math", "Assignments"),
+                new Course ("4", "Web development", "Assignments"),
+            };
+
+    }
+}
